afcf942991097b95c2841fd1f8238653287c188b
Sophie Bunar <sbunar27@hanoverstudents.org>
2025-09-19 12:46:51 -0400

    started pt 2 of day 1

--- AOC-2022/day1.py
+++ AOC-2022/day1.py
@@ -1,4 +1,29 @@
-file = open("day1-test.txt", "r")
+max = 0
+current = 0
+largest_nums = []
+
+file = open('day1.txt', 'r')
 lines = file.readlines()
+
+# PART ONE
 for line in lines:
-    print(line)
+    line = line.strip()
+
+    if line != '':
+        # It's not blank, add to the running total
+        current += int(line)
+    else:
+        # It's blank, update the max (if necessary) and reset the current (always)
+        if current > max:
+            max = current
+            # PART TWO
+            largest_nums.append(max)
+        current = 0
+    
+
+print('Part one: ' + str(max))
+
+# DOES NOT WORK
+maxes = sorted(largest_nums)
+sum_maxes = maxes[-1] + maxes[-2] + maxes[-3]
+print('Part two: ' + str(sum_maxes))
