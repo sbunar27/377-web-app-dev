7bcf0e293e23bff48211a033824f309d7bd8a847
sophie b <sbunar27@hanoverstudents.org>
2025-09-24 20:24:16 -0400

    started tripleM function, molecules dont work.

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -6,10 +6,11 @@ function calculatePercentComposition() {
     
     let percentComposition = ((elementMass*elementAmount)/compoundMass)*100
     let roundedComposition = Math.round(percentComposition*100) / 100
+
     if (percentComposition > 0 && percentComposition <= 100) {
         $('#percent-composition').html(roundedComposition + '% ' + elementName)
     } else {
-        $('#percent-composition').html('Error. Please check your input values.')
+        $('#percent-composition').html('Error. Please check your input values and try again.')
     }
 }
 
@@ -20,9 +21,41 @@ function calculateAbsError() {
 
     let absoluteError = Math.abs(errorNum)
     let roundedAbsError = Math.round(absoluteError*1000) /1000
+
     $('#absolute-error').html(roundedAbsError)
 }
 
 function calculatePercentError() {
-    
+    let absError = $('#abs-error').val()
+    let trueVal = $('#true-value').val()
+
+    let percentErr = (absError/trueVal)*100
+    let percentError = Math.abs(percentErr)
+    let roundedPercentErr = Math.round(percentError*100) / 100
+
+    if (percentError > 0) {
+        $('#percent-error').html(roundedPercentErr + '% Error')
+    } else {
+        $('#percent-error').html('Error. Please check your input values and try again.')
+    }
+}
+
+function calculateTripleM() {
+    let mass = $('#mass').val()
+    let molarMass = $('#mol-mass').val()
+    let mol = $('#mol').val()
+    let units = $('#units').val()
+
+    for (let i = 0; i < 3; i++) {
+        if (mass == '') {
+            $('#mass').val(molarMass*mol + ' g')
+        } else if (mol == '') {
+            let moles = Math.round((mass/molarMass)*1000)/1000
+            $('#mol').val(moles)
+        // THIS PART IS BROKEN
+        } else if (units == '') {
+            let unitNum = Math.round((molarMass*moles)*1000)/1000
+            $('#units').val(unitNum)
+        }
+    }
 }
