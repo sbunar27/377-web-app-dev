083990a544ccd376c71d04a64cfb012795286033
sbunar <s.bunar927@gmail.com>
2026-05-22 11:12:00 -0400

    little fixes

--- 11-final-project/web/delete.php
+++ 11-final-project/web/delete.php
@@ -8,14 +8,11 @@ if (isset($_GET['id'])) {
         die("Connection failed: " . $connection->connect_error);
     }
 
-    // 1. Sanitize the ID (ensure it's just a number)
     $ev_id = intval($_GET['id']);
 
-    // 2. Run the delete query
     $sql = "DELETE FROM events WHERE ev_id = $ev_id";
 
     if ($connection->query($sql)) {
-        // 3. Success! Redirect back to the agenda. 
         header("Location: index.php"); 
         exit();
     } else {
